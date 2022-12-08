4f46e848eea2785433060275fc4fab91e8291af4
mikkot666 <t1tami02@students.oamk.fi>
2022-12-08 16:33:00 +0200

    toimivat kirjautumisnapit ja ajastimien aloituskohta

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
 
-app.use(authenticateToken);
+//app.use(authenticateToken);
 
 app.use('/users', usersRouter);
 app.use('/tili', tiliRouter);
